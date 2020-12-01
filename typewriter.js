74aa493b06b9aaabb0c66795206e300920dff1b8
Samicap <lovestochasewindmills@gmail.com>
2020-12-01 20:53:20 +0000

    working typerwriter.js

--- typewriter.js
+++ typewriter.js
@@ -1,9 +1,14 @@
 const sentence = "hello to the world";
+let delay = 0;
 for (let char of sentence) {
-  let newSentence = "";
   setTimeout(() => {
     process.stdout.write(char) // similar to console.log but prints the characters on the same line rather than next line.
-  }, 1000);
+  }, delay);
+delay += 100;
 
 };
+
+setTimeout(() => {
+  process.stdout.write("\n")
+}, delay);
 
